04786694a1092ecf26985fdb54211b2a4b67e1ab
rybinali <rybinaaaaa.a@gmail.com>
2024-01-16 14:21:35 +0100

    realize own server

--- src/main/java/org/rybina/SocketRunner.java
+++ src/main/java/org/rybina/SocketRunner.java
@@ -7,6 +7,7 @@ import java.io.OutputStream;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SocketRunner {
 
@@ -15,13 +16,16 @@ public class SocketRunner {
 //        https - 443
 //        tcp port
 //        try (Socket socket = new Socket("google.com", 80);
-        try (Socket socket = new Socket(Inet4Address.getByName("google.com"), 80);
+        try (Socket socket = new Socket(Inet4Address.getByName("localhost"), 7777);
              DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
-             DataInputStream inputStream = new DataInputStream(socket.getInputStream())
+             DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+             Scanner scanner = new Scanner(System.in)
         ) {
-            outputStream.writeUTF("Hello world!");
-            byte[] response = inputStream.readAllBytes();
-            System.out.println(response.length);
+            while (scanner.hasNext()) {
+                String request = scanner.nextLine();
+                outputStream.writeUTF(request);
+                System.out.println("Response from server: " + inputStream.readUTF());
+            }
         }
     }
 }
---
+++ src/main/java/org/rybina/SocketServerRunner.java
@@ -0,0 +1,30 @@
+package org.rybina;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SocketServerRunner {
+
+    public static void main(String[] args) throws IOException  {
+        try (ServerSocket serverSocket = new ServerSocket(7777);
+//      serverSocket.accept() - принимает только одно соединение с клиентом, далее это будет решаемо с помощью многопоточности
+//             socket - первый клиент, что подключился
+             Socket socket = serverSocket.accept();
+             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+             DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+             Scanner scanner = new Scanner(System.in)
+        ) {
+            String request = inputStream.readUTF();
+            while (!"stop".equals(request)) {
+                System.out.println("Client request: " + request);
+                String response = scanner.nextLine();
+                outputStream.writeUTF(response);
+                request = inputStream.readUTF();
+            }
+        }
+    }
+}
