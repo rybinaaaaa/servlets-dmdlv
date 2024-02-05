be4b62af2606e98c97ca7f1eb856033578fa0f58
rybinali <rybinaaaaa.a@gmail.com>
2024-02-05 18:21:02 +0100

    add downloading an existing file

--- src/com/rybina/http/servlet/DownloadServlet.java
+++ src/com/rybina/http/servlet/DownloadServlet.java
@@ -7,19 +7,24 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 @WebServlet("/download")
 public class DownloadServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setHeader("Content-Disposition", "attachment; filename=\"filename.txt\"");
-        resp.setContentType("text/plain");
+        resp.setContentType("application/json");
         resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
 
-        try (PrintWriter writer = resp.getWriter()) {
-            writer.write("Hello from servlet");
+        try (OutputStream writer = resp.getOutputStream();
+             InputStream stream = DownloadServlet.class.getClassLoader().getResourceAsStream("first.json");
+        ) {
+            writer.write(stream.readAllBytes());
         }
     }
 }
