074719317aafc3a3cac2490c4c23efceb6479418
rybinali <rybinaaaaa.a@gmail.com>
2024-02-05 17:50:32 +0100

    add servlet basic functional

--- src/com/rybina/http/servlet/FirstServlet.java
+++ src/com/rybina/http/servlet/FirstServlet.java
@@ -9,8 +9,11 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
+import java.util.stream.Stream;
 
 @WebServlet("/first")
 public class FirstServlet extends HttpServlet {
@@ -29,8 +32,29 @@ public class FirstServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
+        req.getContentType();
+        req.getHeader("user-agent");
+
+        String param = req.getParameter("param");
+        Map<String, String[]> parameterMap = req.getParameterMap();
+
+        System.out.println();
+
+        var names = req.getHeaderNames();
+        while (names.hasMoreElements()) {
+            var header = names.nextElement();
+            System.out.println(req.getHeader(header));
+        }
         try (PrintWriter writer = resp.getWriter()) {
             writer.write("<h1> hello from first servlet");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (BufferedReader reader = req.getReader();
+            Stream<String> lines = reader.lines()) {
+            lines.forEach(System.out::println);
         }
     }
 
