b726e900e63b079b886f3a73ff25506b1d1fd165
rybinali <rybinaaaaa.a@gmail.com>
2024-02-05 18:09:40 +0100

    add downloading file, created in real time of programm

---
+++ src/com/rybina/http/servlet/DownloadServlet.java
@@ -0,0 +1,25 @@
+package com.rybina.http.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet("/download")
+public class DownloadServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader("Content-Disposition", "attachment; filename=\"filename.txt\"");
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.write("Hello from servlet");
+        }
+    }
+}
