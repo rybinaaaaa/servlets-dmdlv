ab76362db2e0798724c0bca1b37f14246333bab5
rybinali <rybinaaaaa.a@gmail.com>
2024-02-07 11:39:47 +0100

    fix JspHelper!

--- src/com/rybina/http/util/JspHelper.java
+++ src/com/rybina/http/util/JspHelper.java
@@ -5,7 +5,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class JspHelper {
 
-    private static String JSP_FORMAT = "/WEB-INF/private/content.jsp";
+    private static String JSP_FORMAT = "/WEB-INF/private/%s.jsp";
 
     public static String getPath(String jspName) {
         return String.format(JSP_FORMAT, jspName);
