517a717cf14b0491a5e3a3d0473312d771728da2
rybinali <rybinaaaaa.a@gmail.com>
2024-02-08 12:10:18 +0100

    additional filters

---
+++ src/com/rybina/http/filter/UnsafeFilter.java
@@ -0,0 +1,25 @@
+package com.rybina.http.filter;
+
+
+import com.rybina.http.dto.CreateUserDto;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/admin")
+public class UnsafeFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        CreateUserDto user = (CreateUserDto) ((HttpServletRequest) servletRequest).getSession().getAttribute("user");
+
+        if (user != null) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            ((HttpServletResponse) servletResponse).sendRedirect("/registration");
+        }
+    }
+}
